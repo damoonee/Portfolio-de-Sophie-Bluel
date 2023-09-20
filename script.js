768943be19083a9f09ff11abee3159148ae6ba59
Manon <manonmarchaufromy@gmail.com>
2023-09-20 12:00:35 +0200

    mise en place photos en dynamique dans la modal

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ function AfficherProjets(worksFiltre) {
 
         newFigure.appendChild(newImage);
         newFigure.appendChild(newFigCaption);
-
         gallery.appendChild(newFigure);
     }
 }
@@ -30,6 +29,7 @@ fetch("http://localhost:5678/api/works")
         console.log(works);
 
         AfficherProjets(works);
+        AfficherProjetsModal();
    });
 
 fetch("http://localhost:5678/api/categories")
@@ -85,3 +85,57 @@ if (token !== null) {
         elements.item(i).style.display = "flex";
     }
 }
+
+const openModal = function (e) {
+    e.preventDefault();
+    console.log(e);
+    modal = document.querySelector(e.target.getAttribute('href'));
+    modal.style.display = null;
+    modal.removeAttribute('aria-hidden');
+    modal.setAttribute('aria-modal', 'true');
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.close-modal').addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+}
+
+const closeModal = function (e) {
+    if (modal === null) return;
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener('click', closeModal);
+    modal.querySelector('.close-modal').removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+    modal = null;
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation();
+}
+
+function AfficherProjetsModal () {
+    let modalContent = document.querySelector('.modalContent');
+    modalContent.innerHTML = "";
+
+    for (let i = 0; i < works.length; i++) {
+        let figure = document.createElement("figure");
+        let figureImage = document.createElement("img");
+        let icone = document.createElement("i");
+        let supprImage = document.createElement("div");
+        figureImage.src = works[i].imageUrl;
+        figureImage.alt = works[i].title;
+        supprImage.classList.add("icone");
+        icone.classList.add("fa-solid");
+        icone.classList.add("fa-trash-can");
+
+        figure.appendChild(figureImage);
+        supprImage.appendChild(icone);
+        figure.appendChild(supprImage);
+        modalContent.appendChild(figure);
+    }
+}
+
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal);
+})
